e6677ace96cdc659f57d4b6c95526aea21199dbf
Marlendyz <marleydyzperezromero@gmail.com>
2024-01-26 11:18:31 +0100

    login y register de workers hecho

--- src/controllers/AuthWorkerController.ts
+++ src/controllers/AuthWorkerController.ts
@@ -1,22 +1,19 @@
 import { Request, Response } from "express";
-// preguntar a rey sobre esto 
-import { CreateWorkersRequestBody, LoginUserRequestBody, TokenData } from "../types/types";
+import { CreateWorkersRequestBody, LoginUserRequestBody } from "../types/types";
 import { Tatuate_workers } from "../models/Tatuate_workers";
 import { AppDataSource } from "../database/data-source";
 import bcrypt from "bcrypt";
 import { StatusCodes } from "http-status-codes";
-import jwt from "jsonwebtoken";
 
 export class AuthWorkController {
   async register(
     req: Request<{}, {}, CreateWorkersRequestBody>,
     res: Response
   ): Promise<void | Response<any>> {
-    const { nickname, first_name, last_name, password, email,  } =
-      req.body;
+    const { nickname, first_name, last_name, password, email } = req.body;
 
     const workerRepository = AppDataSource.getRepository(Tatuate_workers);
-    
+
     try {
       const newArtist: Tatuate_workers = {
         nickname,
@@ -32,49 +29,49 @@ export class AuthWorkController {
         message: "Artist created succesfully",
       });
     } catch (error) {
-       res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
-         message: "Error while creating Artist",
-       });
-     }
+      res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+        message: "Error while creating Artist",
+      });
+    }
   }
-  async login(req: Request<{}, {}, LoginUserRequestBody>, res: Response): Promise<void | Response<any>> {
-    
-        const { password, email,} = req.body;
-    
-        const workerRepository = AppDataSource.getRepository(Tatuate_workers);
-        try {
-          if(!email || !password){
-            return res.status(StatusCodes.BAD_REQUEST).json({
-              message: "Email or password is required"
-            });
+  async login(
+    req: Request<{}, {}, LoginUserRequestBody>,
+    res: Response
+  ): Promise<void | Response<any>> {
+    const { password, email } = req.body;
 
-          }
-          const user = await workerRepository.findOne({
-            where: {
-              email: email,
-            },
-          });
-          if(!user){
-            return res.status(StatusCodes.BAD_REQUEST).json({
-              message: "Bad email or password"
-            });
-          }
-          const isPasswordValid = bcrypt.compareSync(password, user.password)
-          
-          if(!isPasswordValid){
-            return res.status(StatusCodes.BAD_REQUEST).json({
-              message: "Bad email or password"
-            });
-          }
-          
-         
-          res.status(StatusCodes.OK).json({
-            message: "login succesfully",
-          });
-        } catch (error) {
-          res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
-            message: "Error while login",
-          });
-        }
+    const workerRepository = AppDataSource.getRepository(Tatuate_workers);
+    try {
+      if (!email || !password) {
+        return res.status(StatusCodes.BAD_REQUEST).json({
+          message: "Email or password is required",
+        });
+      }
+      const user = await workerRepository.findOne({
+        where: {
+          email: email,
+        },
+      });
+      if (!user) {
+        return res.status(StatusCodes.BAD_REQUEST).json({
+          message: "Bad email or password",
+        });
+      }
+      const isPasswordValid = bcrypt.compareSync(password, user.password);
+
+      if (!isPasswordValid) {
+        return res.status(StatusCodes.BAD_REQUEST).json({
+          message: "Bad email or password",
+        });
+      }
+
+      res.status(StatusCodes.OK).json({
+        message: "login succesfully",
+      });
+    } catch (error) {
+      res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+        message: "Error while login",
+      });
+    }
   }
 }
--- src/router.ts
+++ src/router.ts
@@ -1,23 +1,20 @@
 import express from "express";
 import userRoutes from "./routes/users.routes";
 import authUserouter from "./routes/AuthUser.router"
-import { AuthWorrouter } from "./router/AuthWorkerrouter";
+import  AuthWorkRoutes from "./routes/AuthWorkers.router";
 
 
 // -----------------------------------------------------------------------------
 
 const router = express.Router();
 
-// User routes
+
 router.use("/authUser", authUserouter);
 router.use("/api/users",userRoutes);
-router.use("/api/users/:id", userRoutes);
+router.use("/authWor", AuthWorkRoutes);
 router.use("/api/users/:id", userRoutes);
 router.use("/api/users/:id", userRoutes);
 
-// artis routes
-
-router.use("/authWorkerrouter", AuthWorrouter);
 
 
 export default router;
